4f39b345c7270ae6f616ef2f3805ae692016840d
kubix97 <51679705+kubix97@users.noreply.github.com>
2021-06-02 13:26:23 +0200

    Init commit

---
+++ WebApi/DAL/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using LotekWebApi.Model;
+
+namespace LotekWebApi.DAL
+{
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions options) : base(options){
+			}
+		public DbSet<Result> Results { get; set; }
+		
+	}
+}
---
+++ WebApi/Model/Result.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+namespace LotekWebApi.Model
+{
+	[Keyless]
+	public class Result
+	{
+		//public string rowid { get; set;}
+		public string Date { get; set; }
+		public string Vals { get; set; }
+	}
+}
